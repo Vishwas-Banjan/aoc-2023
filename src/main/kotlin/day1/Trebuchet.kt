41cc4ebb83a61dbfda7cf0b682254605bc326577
Vish Banjan <vbanjan@microsoft.com>
2023-12-01 15:09:25 -0800

    refactor

--- src/main/kotlin/day1/Trebuchet.kt
+++ src/main/kotlin/day1/Trebuchet.kt
@@ -2,71 +2,46 @@ package day1
 
 object Trebuchet {
     fun processCalibrationValue(input: String): Int {
-        val calibrationTextArray = input.lines()
-        var total = 0
-        calibrationTextArray.forEach {
-            total += getCalibrationValueForLine(it)
-        }
-        return total
+        return input.lines().sumOf { getCalibrationValueForLine(it) }
     }
 
     private fun getCalibrationValueForLine(input: String): Int {
         val indexOfFirstDigit = input.indexOfFirst { it.isDigit() }
-        val firstDigit = if (indexOfFirstDigit >= 0) input[indexOfFirstDigit] else -1
+        val firstDigit = (if (indexOfFirstDigit >= 0) input[indexOfFirstDigit] else -1).toString().toInt()
 
         val findFirstSpelledDigitRes = input.firstSpelledDigitIndexed()
 
         val indexOfLastDigit = input.indexOfLast { it.isDigit() }
-        val lastDigit = if (indexOfLastDigit >= 0) input[indexOfLastDigit] else -1
+        val lastDigit = (if (indexOfLastDigit >= 0) input[indexOfLastDigit] else -1).toString().toInt()
 
         val findLastSpelledDigitRes = input.lastSpelledDigitIndexed()
 
-        val firstNumRes = firstValidPair(indexOfFirstDigit to firstDigit.toString().toInt(), findFirstSpelledDigitRes)
+        val firstNumRes = firstValidPair(indexOfFirstDigit to firstDigit, findFirstSpelledDigitRes)
         val firstNum = firstNumRes.second
 
-        val lastNumRes = lastValidPair(indexOfLastDigit to lastDigit.toString().toInt(), findLastSpelledDigitRes)
+        val lastNumRes = lastValidPair(indexOfLastDigit to lastDigit, findLastSpelledDigitRes)
         val lastNum = lastNumRes.second
 
         return (firstNum * 10) + lastNum
     }
 
     private fun firstValidPair(thiz: Pair<Int, Int>, that: Pair<Int, Int>): Pair<Int, Int> {
-        return when {
-            thiz.first >= 0 && that.first < 0 -> {
-                thiz
-            }
-
-            thiz.first < 0 && that.first >= 0 -> {
-                that
-            }
-
-            thiz.first >= 0 && that.first >= 0 -> {
-                if (thiz.first < that.first) thiz else that
-            }
-
-            else -> {
-                throw Exception("No valid pair found")
-            }
-        }
+        return findValidPair(thiz, that) { a, b -> a < b }
     }
 
     private fun lastValidPair(thiz: Pair<Int, Int>, that: Pair<Int, Int>): Pair<Int, Int> {
-        return when {
-            thiz.first >= 0 && that.first < 0 -> {
-                thiz
-            }
+        return findValidPair(thiz, that) { a, b -> a > b }
+    }
 
-            thiz.first < 0 && that.first >= 0 -> {
-                that
-            }
-
-            thiz.first >= 0 && that.first >= 0 -> {
-                if (thiz.first > that.first) thiz else that
-            }
-
-            else -> {
-                throw Exception("No valid pair found")
-            }
+    private fun findValidPair(
+        thiz: Pair<Int, Int>,
+        that: Pair<Int, Int>,
+        compare: (Int, Int) -> Boolean
+    ): Pair<Int, Int> {
+        return if (that.first < 0 || compare(thiz.first, that.first)) {
+            thiz
+        } else {
+            that
         }
     }
 
