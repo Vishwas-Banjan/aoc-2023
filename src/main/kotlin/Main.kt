1c704534626d83946b454a841f7e240d4b3d3b74
Vish Banjan <vbanjan@microsoft.com>
2023-12-07 11:39:57 -0800

    day3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 import day1.Trebuchet
 import day2.CubeConundrum
 import day2.DAY_2_INPUT
-import day2.DAY_2_TEST_INPUT
+import day3.DAY_3_INPUT
+import day3.DAY_3_TEST_INPUT
+import day3.GearRatios
 
 fun main(args: Array<String>) {
-    day2()
+    day3()
 }
 
 private fun day1() {
@@ -18,4 +20,11 @@ private fun day2(){
 
     val part2 = CubeConundrum.sumPowerSets(DAY_2_INPUT)
     println("sum of all of the power sets $part2")
+}
+
+private fun day3(){
+    val value = GearRatios.sumOfEnginePartsSchematic1(DAY_3_INPUT)
+    println("Part 1: sum of all of the engine parts $value")
+    val part2 = GearRatios.sumOfEnginePartsSchematic2(DAY_3_INPUT, '*')
+    println("Part 2: sum of all of the engine parts $part2")
 }
---
+++ src/main/kotlin/day3/GearRatios.kt
@@ -0,0 +1,149 @@
+package day3
+
+object GearRatios {
+    fun sumOfEnginePartsSchematic1(input: String): Int {
+        val inputArray = inputTo2DArray(input)
+        return getValidPartNumbersSum(inputArray)
+    }
+
+    fun sumOfEnginePartsSchematic2(input: String, conditionChar: Char): Int {
+        val inputArray = inputTo2DArray(input)
+        return getValidPartNumbersSum(inputArray, conditionChar)
+    }
+
+    private fun getValidPartNumbersSum(input: Array<CharArray>, conditionChar: Char): Int {
+        val partNumCoords = mapPartNumCoords(input)
+        var validPartTotal = 0
+        input.forEachIndexed { rowIndex, chars ->
+            chars.forEachIndexed { columnIndex, c ->
+                if (c == conditionChar) {
+                    var totalAdjacent = 0
+                    var valueTotal = 1
+                    val adjacentCoords = getAdjacentCoordinates(rowIndex, columnIndex)
+
+                    adjacentCoords.map { (row, column) ->
+                        partNumCoords[row to column]
+                    }.distinct().forEach { numberCoords ->
+                        if (numberCoords != null) {
+                            totalAdjacent++
+                            val _row = numberCoords.first
+                            val _columnIndexRange = numberCoords.second
+                            val numberValue = input[_row].slice(_columnIndexRange).joinToString("").toInt()
+                            valueTotal *= numberValue
+                        }
+                    }
+                    if (totalAdjacent == 2) validPartTotal += valueTotal
+                }
+            }
+        }
+        return validPartTotal
+    }
+
+    private fun getValidPartNumbersSum(input: Array<CharArray>): Int {
+        val partNumCoords = mapPartNumCoords(input)
+
+        return partNumCoords.values.distinct().sumOf { (row, columnIndexRange) ->
+            if (hasAdjacentSymbols(columnIndexRange, input, row)) {
+                input[row].slice(columnIndexRange).joinToString("").toInt()
+            } else 0
+        }
+    }
+
+    private fun mapPartNumCoords(input: Array<CharArray>): MutableMap<Pair<Int, Int>, Pair<Int, IntRange>> {
+        val partNumCoords =
+            mutableMapOf<Pair<Int, Int>, Pair<Int, IntRange>>() // (row, column) -> (row, columnIndexRange)
+
+        input.forEachIndexed { rowIndex, chars ->
+            var startIndex: Int
+            var endIndex: Int
+            var columnIndex = 0
+
+            while (columnIndex < chars.size) {
+                var subColumnIndex = columnIndex
+                val char = chars[columnIndex]
+                if (char.isDigit()) {
+                    startIndex = columnIndex
+                    endIndex = columnIndex
+
+                    while (subColumnIndex < chars.size && chars[subColumnIndex].isDigit()) {
+                        endIndex = subColumnIndex
+                        subColumnIndex++
+                    }
+
+                    val columnIndexRange = startIndex..endIndex
+                    columnIndexRange.forEach {
+                        partNumCoords[rowIndex to it] = rowIndex to columnIndexRange
+                    }
+
+                    columnIndex = subColumnIndex
+                } else columnIndex++
+            }
+        }
+
+        return partNumCoords
+    }
+
+    private fun hasAdjacentSymbols(indexRange: IntRange, input: Array<CharArray>, rowIndex: Int): Boolean {
+        for (columnIndex in indexRange) {
+            val res = input.getValidValues(*getAdjacentCoordinates(rowIndex, columnIndex).toTypedArray())
+                .any { !it.isDigit() && it != '.' }
+            if (res) return true
+        }
+        return false
+    }
+
+    private fun getAdjacentCoordinates(row: Int, column: Int): List<Pair<Int, Int>> {
+        return listOf(
+            row to column - 1, // left
+            row to column + 1, // right
+            row - 1 to column, // top
+            row - 1 to column - 1, // top left
+            row - 1 to column + 1, // top right
+            row + 1 to column, // bottom
+            row + 1 to column - 1, // bottom left
+            row + 1 to column + 1 // bottom right
+        )
+    }
+
+    private fun Array<CharArray>.getValidValues(vararg rowToColumnIndex: Pair<Int, Int>): List<Char> {
+        val validValues = mutableListOf<Char>()
+
+        rowToColumnIndex.forEach { (rowIndex, columnIndex) ->
+            val value = this.getValidValueOrNull(rowIndex, columnIndex)
+            if (value != null) {
+                validValues.add(value)
+            }
+        }
+
+        return validValues
+    }
+
+    private fun Array<CharArray>.getValidValueOrNull(row: Int, column: Int): Char? {
+        val rows = this.size
+        val columns = this[0].size
+
+        if (row < 0 || row >= rows || column < 0 || column >= columns) {
+            return null
+        }
+
+        return this[row][column]
+    }
+
+    private fun inputTo2DArray(input: String): Array<CharArray> {
+        val lines = input.lines()
+        val rows = lines.size
+        val columns = lines[0].length
+
+        // Create a 2D array of characters
+        val twoDArray = Array(rows) { CharArray(columns) { ' ' } }
+
+        // Initialize the 2D array with characters from the input string
+        for ((i, line) in lines.withIndex()) {
+            for ((j, char) in line.withIndex()) {
+                twoDArray[i][j] = char
+            }
+        }
+
+        return twoDArray
+    }
+}
